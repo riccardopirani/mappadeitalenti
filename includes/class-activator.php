153acbb41e31cdf58904f407e88d88e1c34c65b2
Riccardo Pirani <riccardo.pirani@reiser.it>
2025-02-24 15:47:45 +0100

    fix code

---
+++ includes/class-activator.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Classe di attivazione
+ */
+namespace map_plugin;
+
+class Activator {
+    public static function attivazione() {
+        // Codice di attivazione qui
+    }
+}
---
+++ includes/class-deactivator.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Classe di disattivazione
+ */
+namespace map_plugin;
+
+class Deactivator {
+
+    public static function disattivazione() {
+        // Azioni da eseguire alla disattivazione del plugin
+    }
+
+}
